9b953c9401a18942b1a557b40084a4fd8ee718a0
NelfM <NelfM@users.noreply.github.com>
2020-09-26 14:22:22 -0700

    Commit 3, lots of changes

--- first.py
+++ first.py
@@ -1,17 +1,24 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello! this is the main page <h1>Hola Amigos<h1>"
+    return render_template("index.html", content="Testing")
 
-@app.route("/<name>")
-def user(name):
-    return f"Hello {name}!"
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = request.form["nm"]
+        
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template("login.html")
 
-@app.route("/admin")
-def admin():
-    return redirect(url_for("user", name = "Admin!"))
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
+
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
