ab489ea5f3eb54466213ea09afaf80386cbdea2d
NelfM <NelfM@users.noreply.github.com>
2020-09-26 15:59:42 -0700

    commit 4

--- first.py
+++ first.py
@@ -3,20 +3,31 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
+    
     return render_template("index.html", content="Testing")
+
+
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
+
     if request.method == "POST":
-        user = request.form["nm"]
-        
-        return redirect(url_for("user", usr=user))
-    else:
-        return render_template("login.html")
+        req = request.form
+
+        name = req["fullname"]
+        email = req["email"]
+        symp = req["symptoms"]
+
+        print(name, email, symp)
+
+        return redirect(url_for("home"))
+    return render_template("login.html")
+
+
 
 @app.route("/<usr>")
-def user(usr):
-    return f"<h1>{usr}</h1>"
+def done(usr):
+    return f"<h1>{usr}<h1>"
 
 
 
